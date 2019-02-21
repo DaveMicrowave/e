00e07ad4d6bb255e594c6135260e92d763c704a4
Jeekkee <vildosolaje@gmail.com>
2019-02-21 20:27:15 +0800

    welcome programmers

---
+++ ClassLibrary7/ClassLibrary7/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ClassLibrary7
+{
+    [ComVisible(true)] // Don't forget 
+    [ClassInterface(ClassInterfaceType.AutoDual)] // these two lines
+    [Guid("485B98AF-53D4-4148-B2BD-CC3920BF0ADF")] // or this GUID
+    public class Class1
+    {
+
+        public String GetDirectory()
+        {
+            string ret = "";
+            Type myType = typeof(Class1);
+            string username = System.Environment.UserName;
+            ret = "C:\\Users\\" + username + "\\source\\repos\\" + myType.Namespace + "\\" + myType.Namespace + "\\bin\\debug\\";
+
+            return ret;
+        }
+
+        public String WriteFiles(String arg2)
+        {
+            string[] files = Directory.GetFiles(GetDirectory(), "*.txt", SearchOption.AllDirectories);
+            string message = arg2;
+            string success = "Success!";
+            foreach (string file in files)
+            {
+                //Console.WriteLine(file + "  Has been written!");
+                File.WriteAllText(file, message);
+            }
+
+            return success;
+        }
+    }
+}
